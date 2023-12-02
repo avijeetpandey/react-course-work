aa9cd38265081e1c4f55cc4fa514bc803b32af80
Avijeet Pandey <avijeetpandey25@gmail.com>
2023-12-03 00:21:54 +0530

    Add: components and fitler part

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
+import Body from "./components/Body";
 import Header from "./components/Header";
 
 export default function App() {
   return (
     <div>
       <Header />
+      <Body />
     </div>
   );
 }
